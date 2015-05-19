cff9617efe64814c17cf4828f43689db9d82b625
Gregory Jefferis <jefferis@gmail.com>
2015-05-19 21:22:23 +0100

    normalise whitespace in line with rstudio project
    * which has 2 spaces not tabs
    * may as well get this out of the way now

--- R/zen_collection_create.R
+++ R/zen_collection_create.R
@@ -4,20 +4,20 @@
 # Method	POST
 # Request headers	Content-Type: application/json
 # Scopes	deposit:write
-# Success response	
+# Success response
 # Code: 201 Created
 # Body: a deposition resource.
 
 #' Create a collection on Zenodo
-#' 
+#'
 #' @param collection_name Name of a new collection to be created on Zenodo
 #' @template access_token
 #' @export
 zen_create <- function(collection_name = NULL, access_token = getOption('zenodo_token')) {
-	assert_that(!is.null(collection_name)) # no empty collection
-	# TODO: Check to see if collection exists
-	create_url <- "https://zenodo.org/api/deposit/depositions"
-	response <- post()
-	results <- content(response)
-	results
+  assert_that(!is.null(collection_name)) # no empty collection
+  # TODO: Check to see if collection exists
+  create_url <- "https://zenodo.org/api/deposit/depositions"
+  response <- POST()
+  results <- content(response)
+  results
 }
