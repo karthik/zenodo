bcfcbbd630d10a61831e6a6a9b227dcfa2e04a3c
Gregory Jefferis <jefferis@gmail.com>
2015-05-20 12:11:23 +0100

    vectorise zen_file_retrieve

--- R/zen_file_retrieve.R
+++ R/zen_file_retrieve.R
@@ -12,7 +12,7 @@
 
 #' Retrieve file description for given deposition and file id
 #'
-#' @param deposition.id,file.id Zenodo ids for deposition and file
+#' @param deposition.id,file.id Zenodo ids for deposition and file (one or many)
 #' @inheritParams zen_collections
 #' @return A data_frame with columns
 #' \itemize{
@@ -23,13 +23,23 @@
 #' \item filename
 #' }
 #' @export
-#' @importFrom dplyr as_data_frame
+#' @importFrom dplyr as_data_frame bind_rows
 #' @examples
 #' \dontrun{
 #' # retrieve info for single file
 #' zen_file_retrieve(29766, "d98a9e02-c9df-4e4a-b643-8f6f1d9eb2d5")
+#'
+#' # retrieve info for all files in current user's collections
+#' # however this info is already in the data_frame returned by
+#' # zen_collections() so this is not a particularly efficient strategy
+#' my_files=with(zen_collections(), zen_file_retrieve(id, files.id))
 #' }
 zen_file_retrieve<-function(deposition.id, file.id, ...) {
+  if(length(deposition.id)>1 || length(file.id)>1) {
+    res=mapply(zen_file_retrieve, deposition.id, file.id, MoreArgs = list(...),
+               SIMPLIFY = F)
+    return(bind_rows(res))
+  }
   path=paste0('api/deposit/depositions/',deposition.id,'/files/',file.id)
   response=zen_fetch(path, ...)
   as_data_frame(response)
