e32d2d00c7dcb99e506b11f387b947cc956fea22
Gregory Jefferis <jefferis@gmail.com>
2015-05-20 12:05:01 +0100

    implement zen_file_retrieve using zen_fetch

--- R/zen_file_retrieve.R
+++ R/zen_file_retrieve.R
@@ -1,11 +1,31 @@
 # Description	Retrieve a single deposition file.
 # URL	https://zenodo.org/api/deposit/depositions/:id/files/:file_id
 # Method	GET
-# URL parameters	
+# URL parameters
 # Required:
 # id: Deposition identifier
 # file_id: Deposition file identifier
-# Success response	
+# Success response
 # Code: 200 OK
 # Body: a deposition file resource.
 # Error response	See HTTP status codes (400 and 500 series errors) and error responses.
+
+#' Retrieve file description for given deposition and file id
+#'
+#' @param deposition.id,file.id Zenodo ids for deposition and file
+#' @inheritParams zen_collections
+#' @return A data_frame with columns
+#' \itemize{
+#'
+#' \item id
+#' \item checksum
+#' \item filesize
+#' \item filename
+#' }
+#' @export
+#' @importFrom dplyr as_data_frame
+zen_file_retrieve<-function(deposition.id, file.id, ...) {
+  path=paste0('api/deposit/depositions/',deposition.id,'/files/',file.id)
+  response=zen_fetch(path, ...)
+  as_data_frame(response)
+}
