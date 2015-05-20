7e8e9f8d2070a2b92197a95cf36c363dd186a08b
Gregory Jefferis <jefferis@gmail.com>
2015-05-20 11:04:11 +0100

    refactor zen_collections to use zen_fetch
    * includes an example of some of the interesting fields
    * this means that it actually works
      nb the Zenodo API presently seems to ask for the PAT to be included in the url
      of all requests rather than in the body of the url (which seems eminently
      sensible)

--- R/zen_collection_list.R
+++ R/zen_collection_list.R
@@ -3,17 +3,18 @@
 #'
 #' List all depositions for the currently authenticated user. Collections
 #' typically archive all outputs of a research project.
-#' @template access_token
+#' @param ... Additional arguments passed to \code{\link{zen_fetch}} to define
+#'   request, authorisation or parsing options.
 #' @export
+#' @return a data.frame (with class \code{dplyr::tbl_df}) containing metadata
+#'   about all existing collections for a user.
+#' @seealso \code{\link{zen_fetch}}
 #' @examples \dontrun{
 #' my_collections <- zen_collections()
+#' my_collections %>%
+#'   select(metadata.title, metadata.upload_type, metadata.doi, files.filename)
 #' }
-zen_collections <- function(access_token = zen_pat()) {
-  dir_path <- "https://zenodo.org/api/deposit/depositions"
-  args <- as.list(c(access_token = access_token))
-  results <- httr::GET(dir_path, query = args)
-  request <- httr::content(results)
+zen_collections <- function(...) {
+  request <- zen_fetch('api/deposit/depositions', ...)
   process_hitter_response(request)
 }
-
-
